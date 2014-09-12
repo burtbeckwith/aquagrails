6c9353e5aa6e3888c696dacc56f626aa38d42fd2
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2014-09-12 23:38:16 +0200

    switched to hibernate 3 because of this bug: GRAILS-11198 https://jira.grails.org/browse/GRAILS-11198

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,7 +63,7 @@ grails.project.dependency.resolution = {
         compile ":asset-pipeline:1.9.6"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        runtime ":hibernate:3.6.10.17" // or ":hibernate4:4.3.5.5"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
