62eaa21e272e70982a61183e0b8e8a7afe20d7b0
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2014-09-22 23:39:24 +0200

    added script to export domain model as java classes this enables us to re-import it into an UML model

---
+++ scripts/ExportDomainModel.groovy
@@ -0,0 +1,49 @@
+import org.codehaus.groovy.grails.commons.DefaultGrailsDomainClass
+import grails.util.GrailsNameUtils
+
+//includeTargets << grailsScript("_Init")
+includeTargets << grailsScript("_GrailsPackage")
+includeTargets << grailsScript("_GrailsBootstrap")
+includeTargets << grailsScript("_GrailsCreateArtifacts")
+
+target(exportDomainModel: "export Grails Domain Model as Java-Code") { 
+    depends(checkVersion, parseArguments, packageApp,loadApp)
+    def convertType =  {def type->
+        def types= ['[B':'Byte[]','[C':'Char[]','[I':'Integer']
+        def res = type.toString()-"class "-"java.lang."-"java.util."-"java.math."
+        res = types[res]?:res
+        if (res.startsWith('[L')) {
+            res = convertType(res[2..-1])
+        }
+        return res
+    }
+    println " starting domain class export ".center(80,"=")
+    def domainClasses = grailsApp.domainClasses
+
+    if (!domainClasses) {
+        println "No domain classes found in grails-app/domain, trying hibernate mapped classes..."
+        bootstrap()
+        domainClasses = grailsApp.domainClasses
+    }
+
+    grailsApp.domainClasses.each { dc ->
+        def code = new StringBuilder()
+        def packageName = dc.clazz.name-".${dc.clazz.simpleName}"
+        code << "package ${packageName};\n\n"
+        code << "public class ${dc.clazz.simpleName} {\n"
+        def domainInstance = new DefaultGrailsDomainClass(grailsApp.classLoader.loadClass("${dc.fullName}"))
+        domainInstance.getPersistentProperties().each {prop ->
+            if (! dc.associationMap[prop.name]) {
+                code << "   public ${convertType(prop.type)} ${prop.name};\n"
+            }
+        }
+        dc.associationMap.each {name,type->
+            code << "   public ${convertType(type)}[] ${name};\n"
+        }
+        code << "\n}\n" 
+        def path = "documentation/domain/"+packageName.replaceAll(/[.]/,'/')+"/"
+        new File(path).mkdirs()
+        new File(path+dc.clazz.simpleName+".java").write(code.toString())
+    }
+}
+setDefaultTarget(exportDomainModel)
